dff09af19d2ced55269310eb70df5d7b62c5dde8
Ankit Shukla <4nkit5hukla@gmail.com>
2021-05-15 16:35:38 +0530

    Final Push 15-05-2021

--- footer.php
+++ footer.php
@@ -1,3 +1,186 @@
+<footer>
+  <div class="footer-top py-5">
+    <div class="container">
+      <div class="row f-menu">
+        <div class="col-12 col-md-3 col-lg-3">
+          <h4 class="f-menu-title">Shoes</h4>
+          <ul class="footer-menu-list p-0 list-unstyled d-none d-md-block">
+            <li>
+              <a href="#" class="text-decoration-none">Sneakers & Shoes</a>
+            </li>
+            <li>
+              <a href="#" class="text-decoration-none">Foltball boots</a>
+            </li>
+            <li>
+              <a href="#" class="text-decoration-none">Running Shoes</a>
+            </li>
+            <li>
+              <a href="#" class="text-decoration-none">Netball Sneakers</a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-12 col-md-3 col-lg-3">
+          <h4 class="f-menu-title">Clothing</h4>
+          <ul class="footer-menu-list p-0 list-unstyled d-none d-md-block">
+            <li>
+              <a href="#" class="text-decoration-none">All Clothing</a>
+            </li>
+            <li>
+              <a href="#" class="text-decoration-none">Tops</a>
+            </li>
+            <li>
+              <a href="#" class="text-decoration-none">Trousers & Leggings</a>
+            </li>
+            <li>
+              <a href="#" class="text-decoration-none">Tracksuits</a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-12 col-md-3 col-lg-3">
+          <h4 class="f-menu-title">Kid's</h4>
+          <ul class="footer-menu-list p-0 list-unstyled d-none d-md-block">
+            <li>
+              <a href="#" class="text-decoration-none">Kid's Sneakers</a>
+            </li>
+            <li>
+              <a href="#" class="text-decoration-none">Kid's Backpacks</a>
+            </li>
+            <li>
+              <a href="#" class="text-decoration-none">Kid's Football</a>
+            </li>
+            <li>
+              <a href="#" class="text-decoration-none">Kid's Running</a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-12 col-md-3 col-lg-3">
+          <h4 class="f-menu-title">Featured</h4>
+          <ul class="footer-menu-list p-0 list-unstyled d-none d-md-block">
+            <li>
+              <a href="#" class="text-decoration-none">New Releases</a>
+            </li>
+            <li>
+              <a href="#" class="text-decoration-none">Backpackes & Bags</a>
+            </li>
+            <li>
+              <a href="#" class="text-decoration-none">Footballs</a>
+            </li>
+            <li>
+              <a href="#" class="text-decoration-none">Goalkeeper gloves</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="footer-bottom py-5">
+    <div class="secondary-container">
+      <div class="row">
+        <div class="col-12 col-md-3">
+          <ul class="footer-menu-list-light p-0 list-unstyled">
+            <li>
+              <a href="#" class="text-decoration-none text-uppercase text-white">Find A Store</a>
+            </li>
+            <li>
+              <a href="#" class="text-decoration-none text-uppercase text-white">Sign up for email</a>
+            </li>
+            <li>
+              <a href="#" class="text-decoration-none text-uppercase text-white">Become a member</a>
+            </li>
+            <li>
+              <a href="#" class="text-decoration-none text-uppercase text-white">Send us feedback</a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-12 col-md-3 footer-widgets">
+          <h5 class="mb-3 text-white text-uppercase">Find A Store</h5>
+          <ul class="footer-menu-list p-0 list-unstyled">
+            <li>
+              <a href="orders.php" class="text-decoration-none">Order Status</a>
+            </li>
+            <li>
+              <a href="#" class="text-decoration-none">Shipping & Delivery</a>
+            </li>
+            <li>
+              <a href="#" class="text-decoration-none">Returns</a>
+            </li>
+            <li>
+              <a href="#" class="text-decoration-none">Payment Options</a>
+            </li>
+            <li>
+              <a href="contact.php" class="text-decoration-none">Contact Us</a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-12 col-md-3 footer-widgets">
+          <h5 class="mb-3 text-white text-uppercase">About Hush</h5>
+          <ul class="footer-menu-list p-0 list-unstyled">
+            <li>
+              <a href="#" class="text-decoration-none">News</a>
+            </li>
+            <li>
+              <a href="#" class="text-decoration-none">Careers</a>
+            </li>
+            <li>
+              <a href="#" class="text-decoration-none">Investors</a>
+            </li>
+            <li>
+              <a href="#" class="text-decoration-none">Sustainability</a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-12 col-md-3 footer-widgets">
+          <ul class="footer-menu-list social-icon p-0 list-unstyled d-block">
+            <li class="d-inline-block">
+              <a href="#" class="text-decoration-none"><i class="fa fa-twitter"></i>
+              </a>
+            </li>
+            <li class="d-inline-block">
+              <a href="#" class="text-decoration-none"><i class="fa fa-facebook"></i>
+              </a>
+            </li>
+            <li class="d-inline-block">
+              <a href="#" class="text-decoration-none"><i class="fa fa-youtube-play"></i>
+              </a>
+            </li>
+            <li class="d-inline-block">
+              <a href="#" class="text-decoration-none"><i class="fa fa-instagram"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="secondary-container border-top-footer">
+      <div class="row below-footer pt-3">
+        <div class="col-12 col-md-6 mb-2">
+          <p>
+            2020 Hush Cannabis Club Inc. All Rights Reserved
+          </p>
+        </div>
+        <div class="col-12 col-md-6 mb-2">
+          <ul class="below-footer-menu text-left text-md-right">
+            <li class="d-block d-md-inline-block">
+              <a class="ml-0 ml-md-2 ml-lg-3" href="#">Guides</a>
+            </li>
+            <li class="d-block d-md-inline-block">
+              <a class="ml-0 ml-md-2 ml-lg-3" href="#">Terms Of Use</a>
+            </li>
+            <li class="d-block d-md-inline-block">
+              <a class="ml-0 ml-md-2 ml-lg-3" href="#">Terms Of Sale</a>
+            </li>
+            <li class="d-block d-md-inline-block">
+              <a class="ml-0 ml-md-2 ml-lg-3" href="#">Company Details</a>
+            </li>
+            <li class="d-block d-md-inline-block">
+              <a class="ml-0 ml-md-2 ml-lg-3" href="#">Privacy & Cookie Policy</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
 <?php wp_footer();?>
 </body>
 
